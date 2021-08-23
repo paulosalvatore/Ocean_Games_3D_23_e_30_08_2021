765c13d3c152385a56e77c6f3e691b68be16b7ad
Paulo Salvatore <salvatoregames@gmail.com>
2021-08-23 11:54:26 -0300

    Movimentação do Player implementada

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody rb;
+
+    public float moveSpeed = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        var v = Input.GetAxis("Vertical") * moveSpeed;
+        var h = Input.GetAxis("Horizontal") * moveSpeed;
+
+        rb.velocity = new Vector3(h, rb.velocity.y, v);
     }
 }
